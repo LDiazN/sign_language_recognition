3489728231299a65414168a7eda40cc224878198
Luis Díaz <ldiazn98@gmail.com>
2022-04-09 19:23:40 -0400

    added initial layout for project

---
+++ usb_slr/src/slr/main.py
@@ -0,0 +1,12 @@
+"""
+    Executable file, write all click functions here.
+"""
+
+import click
+
+
+@click.group()
+def usb_slr():
+    """
+        Gloss-level Sign language recognition app
+    """
